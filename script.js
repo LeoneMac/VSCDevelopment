d65d7dac526105d4e3ee24a561cf166f9c9d3d7c
Leonardo Machado <leobatista2807@gmail.com>
2022-08-20 15:44:08 -0300

    Shit implementation of Gliderjs

--- script.js
+++ script.js
@@ -14,3 +14,15 @@
         document.getElementById('censorshipBox').value = userCensoredOutput;
     }
 }
+
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.glider'), {
+        slidesToShow: 1,
+        dots: '#dots',
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev',
+          next: '.glider-next'
+        }
+      });
+  })
