6a551cef8edc04208417a4409e67ad6fad1c97ea
Leonardo Machado <leobatista2807@gmail.com>
2022-08-20 22:04:06 -0300

    Backup

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 {
     function censorSwearing(){
-        // Here we call the text area implemented that will receive the user input
         const textInput = document.getElementById('censorshipBox');
-        // With / we can start the regular expression, separating our prohibited words with | and ending
-        // the list with / again, '\b' tells that swears are only the whole words, the 'gi' flags tell that
-        // our list is global and case insensitive, so ass and Ass are both censored
+        const censoredWordBackup = textInput.value;
         let swears = /\bass\b|\bshit\b|\bjerk\b|\bfuck\b|\bfaggot\b|\bcorinthians\b|\bcu\b/gi;
-        // Declaring the variable that get the textarea value 
         let userInput = textInput.value;
-        // Declaring the variable that search for any bad words in our regex and replace them with "****"
         let userCensoredOutput = userInput.replace(swears, "****");
-        // Here we get the textarea again and implement the previous variable
         document.getElementById('censorshipBox').value = userCensoredOutput;
+        if (swears.test = false){
+            let inputValue = censoredWordBackup;
+            
+            document.getElementById('censorshipBox').value = inputValue;
+        }
     }
     
-    function atraso(){
-    let delay = 2000;
-    setTimeout(censorSwearing, delay); 
-    }
+    // function atraso(){
+    //     let testRegex = censorSwearing.userInput;
+    //     let outputTestRegex = testRegex.value;
+    //     console.log(outputTestRegex); 
+    //     let delay = 2000;
+    //     setTimeout(censorSwearing, delay); 
+    // }
 }
 
 {
